922aa561669bd1587f041ae82cbeeb4ddcd63034
Andri <andristwan28@gmail.com>
2025-01-18 11:52:47 +0700

    implement CreateUser and GetUserByName methods with transaction handling.

--- apps/backend/cmd/internal/repository/auth.go
+++ apps/backend/cmd/internal/repository/auth.go
@@ -4,9 +4,10 @@ import (
 	"context"
 
 	"github.com/anddriii/KitaRecipes/cmd/internal/model/domain"
+	"gorm.io/gorm"
 )
 
 type AuthRepo interface {
-	CreateUSer(ctx context.Context, name string, photo string, username string, email string, hasPassword string, role string)
-	GetUserByName(ctx context.Context, usernamename string) (*domain.Login, error)
+	CreateUSer(ctx context.Context, db *gorm.DB, name string, photo string, username string, email string, hasPassword string, role string) error
+	GetUserByName(ctx context.Context, db *gorm.DB, username string) (*domain.Login, error)
 }
--- apps/backend/cmd/internal/repository/auth_impl.go
+++ apps/backend/cmd/internal/repository/auth_impl.go
@@ -2,8 +2,10 @@ package repository
 
 import (
 	"context"
+	"errors"
 
 	"github.com/anddriii/KitaRecipes/cmd/internal/model/domain"
+	"gorm.io/gorm"
 )
 
 type AuthRepositoryImpl struct {
@@ -14,11 +16,42 @@ func NewAuthRepository() AuthRepo {
 }
 
 // CreateUSer implements AuthRepo.
-func (a *AuthRepositoryImpl) CreateUSer(ctx context.Context, name string, photo string, username string, email string, hasPassword string, role string) {
-	panic("unimplemented")
+func (repo *AuthRepositoryImpl) CreateUSer(ctx context.Context, db *gorm.DB, name string, photo string, username string, email string, hasPassword string, role string) error {
+	tx := db.Begin()
+
+	//insert ke tabel author
+	author := domain.RecipeAuthor{Name: name, Photo: photo}
+	if err := tx.Create(&author).Error; err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	//insert ke tabel logins
+	login := domain.Login{
+		AuthorId: int(author.ID),
+		Username: username,
+		Email:    email,
+		Password: hasPassword,
+		Role:     role,
+	}
+
+	if err := tx.Create(&login).Error; err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	return tx.Commit().Error
 }
 
 // GetUserByName implements AuthRepo.
-func (a *AuthRepositoryImpl) GetUserByName(ctx context.Context, usernamename string) (*domain.Login, error) {
-	panic("unimplemented")
+func (repo *AuthRepositoryImpl) GetUserByName(ctx context.Context, db *gorm.DB, username string) (*domain.Login, error) {
+	var user domain.Login
+	err := db.Preload("Author").Where("username = ", username).First(&user).Error
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, errors.New("user not found")
+		}
+		return nil, err
+	}
+	return &user, nil
 }
